e880ba49fb59cb004c2e3bd737d6207232348c9d
jdwil <me@jdwilliams.xyz>
2021-05-10 10:13:42 -0500

    Bug fix

--- src/firefly_integration/domain/service/marshal_dataframe.py
+++ src/firefly_integration/domain/service/marshal_dataframe.py
@@ -13,7 +13,7 @@ import numpy as np
 class MarshalDataframe(ff.DomainService):
     def __call__(self, df: pd.DataFrame, table: domain.Table) -> pd.DataFrame:
         for column in table.columns:
-            if column.data_type in (date, datetime):
+            if column.data_type in (date, datetime) and column.name in df:
                 df[column.name] = pd.to_datetime(df[column.name])
 
         return df

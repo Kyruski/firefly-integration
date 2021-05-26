87ebdafca19736b3e45fe40cb6a9d26061e11734
jdwil <me@jdwilliams.xyz>
2021-05-26 14:58:02 -0500

    Convert to json

--- src/firefly_integration/application/service/query_data.py
+++ src/firefly_integration/application/service/query_data.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 import firefly as ff
+import pandas as pd
+
 import firefly_integration.domain as ffi
 
 
@@ -10,4 +12,6 @@ class QueryData(ff.ApplicationService):
     _query_warehouse: ffi.QueryWarehouse = None
 
     def __call__(self, sql: str, **kwargs):
-        return self._query_warehouse(sql)
+        ret: pd.DataFrame = self._query_warehouse(sql)
+
+        return ret.to_json(orient='records')

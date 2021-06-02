e966b906ec9e90b3c4830a26e5086e771b07203b
jdwil <me@jdwilliams.xyz>
2021-06-02 09:42:28 -0500

    Do not set an index after removing duplicates.

--- src/firefly_integration/domain/service/remove_duplicates.py
+++ src/firefly_integration/domain/service/remove_duplicates.py
@@ -11,7 +11,5 @@ class RemoveDuplicates(ff.DomainService):
         try:
             df.sort_values(table.duplicate_sort, inplace=True)
             df.drop_duplicates(subset=table.duplicate_fields, keep='last', inplace=True)
-            if 'id' in df:
-                df.set_index(['id'], inplace=True)
         except IndexError:
             pass

d3416e56af5915cd802932559acba298156170d6
jdwil <me@jdwilliams.xyz>
2021-08-17 08:51:06 -0500

    Check the files in an entire table and scan them for partitions that need to be compacted. This should save quite a few s3 list operations

--- src/firefly_integration/application/service/compact.py
+++ src/firefly_integration/application/service/compact.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
+import awswrangler as wr
 import firefly as ff
-from botocore.exceptions import ClientError
 
 import firefly_integration.domain as domain
 
@@ -13,32 +13,32 @@ class Compact(ff.ApplicationService):
     _context: str = None
 
     def __call__(self, table_name: str = None, path: str = None, **kwargs):
-        # Run compaction on all tables
+        # Run compaction on all tables / partitions
         if table_name is None:
             for table in self._catalog_registry.get_all_tables():
-                self.invoke(f'{self._context}.Compact', {
-                    'table_name': table.name,
-                }, async_=True)
-
-        # Run compaction on all partitions for the given table
-        elif path is None:
-            table = self._catalog_registry.get_table(table_name)
-            if table is not None and len(table.partitions) > 0 or table.time_partitioning is not None:
-                try:
-                    for partition in self._dal.get_partitions(table):
-                        self.invoke(f'{self._context}.Compact', {
-                            'table_name': table.name,
-                            'path': partition,
-                        }, async_=True)
-                except ClientError as e:
-                    self.info(str(e))
-            elif table is not None:
-                self.invoke(f'{self._context}.Compact', {
-                    'table_name': table.name,
-                    'path': table.full_path(),
-                }, async_=True)
+                if len(table.partitions) > 0 or table.time_partitioning is not None:
+                    self._scan_partitions(table)
+                else:
+                    self.invoke(f'{self._context}.Compact', {
+                        'table_name': table.name,
+                        'path': table.full_path(),
+                    }, async_=True)
 
         # Run compaction on single partition
-        else:
+        elif path is not None:
             table = self._catalog_registry.get_table(table_name)
             self._dal.compact(table=table, path=path)
+
+    def _scan_partitions(self, table: domain.Table):
+        files = wr.s3.list_objects('s3://' + table.full_path())
+        partitions = []
+        for file in files:
+            if file.endswith('.dat.snappy.parquet'):
+                continue
+            partition = '/'.join(file.split('/')[:-1])
+            if partition not in partitions:
+                partitions.append(partition)
+                self.invoke(f'{self._context}.Compact', {
+                    'table_name': table.name,
+                    'path': partition,
+                }, async_=True)
